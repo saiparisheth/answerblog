bd59d0f005144baf4811aaf619816a01399378ea
MoneshMS <moneshsanju@gmail.com>
2022-02-14 06:09:20 +0530

    Modified available courses

--- src/app/home/courses/courses.component.ts
+++ src/app/home/courses/courses.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
+  courses = [
+    { courseName: 'Ethical Hacking', description: 'week 3 updated' },
+    { courseName: 'Ethical Hacking', description: 'week 3 updated' },
+    { courseName: 'Ethical Hacking', description: 'week 3 updated' },
+    { courseName: 'Ethical Hacking', description: 'week 3 updated' },
+    { courseName: 'Ethical Hacking', description: 'week 3 updated' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
-  imports: [MatIconModule, MatButtonModule],
-  exports: [MatIconModule, MatButtonModule],
+  imports: [MatIconModule, MatButtonModule, MatCardModule, MatDividerModule],
+  exports: [MatIconModule, MatButtonModule, MatCardModule, MatDividerModule],
 })
 export class MaterialModule {}
